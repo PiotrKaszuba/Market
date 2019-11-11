95f6cd9cc9441e33f9ffeec14a07445cc5bb00ce
Piotr <piotrkaszuba1996@gmail.com>
2019-11-11 20:17:09 +0100

    Trader tasks

--- src/market/relogo/Trader.groovy
+++ src/market/relogo/Trader.groovy
@@ -21,34 +21,45 @@ class Trader extends ReLogoTurtle {
 	int thirst
 	int travelCost = 1
 	int needThreshold = 25
+	def task = null
+	def taskSteps = null
+	def miningFatigue = 10
+	int ambition
 	boolean alive = true
 	String state = 'free'
-	def construct(int id, int rice, int water, int gold, int riceNeed, int waterNeed) {
+	def construct(int id, int rice, int water, int gold, int riceNeed, int waterNeed, int ambition) {
 		this.id = id
 		this.rice = rice
 		this.water = water
 		this.gold = gold
 		this.hunger = riceNeed
 		this.thirst = waterNeed
+		this.ambition = ambition
 	}
 	
 	def finishedTask() {
-		
+		task = null
 	}
 	
 	
 	
 	
-	def panic() {
+	def panic(globalPrice) {
 		def panicRes
 		def otherResourceRisk
+		def panicResConsumeAmount
+		def otherResConsumeAmount
 		if(water<=rice) {
 			panicRes = 'water'
 			otherResourceRisk = sqrt(max(1, 1.2*panicThreshold-rice))
+			panicResConsumeAmount = water*100+thirst
+			otherResConsumeAmount = rice*100+hunger
 		}
 		else {
 			panicRes = 'rice'
 			otherResourceRisk = (panicThreshold*1.2)/(rice+1)
+			panicResConsumeAmount = rice*100+hunger
+			otherResConsumeAmount = water*100+thirst
 		}
 		
 		List<TaskAttractiveness> taskAtts = []
@@ -63,17 +74,65 @@ class Trader extends ReLogoTurtle {
 				taskAtts.add(new TaskAttractiveness(attractiveness, cost, gain, it, panicRes))
 			}
 			if(it instanceof Market && it.alive) {
-				def gain = 
-				
+				def gain = it.meanAmountSoldPerStep(panicRes) + 0.2 * it.meanOfResourceLft(panicRes)
+				def priceDiff = globalPrice[panicRes]-it.discountedMeanPrice(panicRes)
+				def attractiveness = (gain - + priceDiff - cost - cost*otherResourceRisk) * (Math.random()/2.5 + 0.6)
+				taskAtts.add(new TaskAttractiveness(attractiveness, cost, gain, it, panicRes))
 			}
+		}
+		taskAtts = taskAtts.sort { it.attractiveness }
+		if(! taskAtts.empty)
+			task = taskAtts.pop()
+		else 
+			task = null
+		while(! checkIfPossibleToFinish(task)) {
+			if(! taskAtts.empty)
+				task = taskAtts.pop()
+			else
+				task = null
+		}
+		if(task==null) return
+		
+		
+		def taskGoal
+		def timeout
+		if (task.target instanceof Market) {
+			taskGoal = 'buy'
+			timeout = 20 + ambition * 3
+		}
+		else {
+			taskGoal = 'mine'
+			timeout = 3 + ambition
+			
+			def timeoutCost = timeout * miningFatigue
+			
+			def otherResLeft = otherResConsumeAmount - task.cost - timeoutCost
+			
+			def timeoutFactor = min(1, otherResLeft/ (panicThreshold*100))
+			
+			timeout = timeoutFactor * timeout
+			
+			
 			
 			
 		}
 		
+		taskSteps = [['do' : 'goto', 'target' : task.target], ['do' : taskGoal, 'timeout' : timeout ] ]
+		
+		
 		
 	}
 	
-	def step() {
+	def checkIfPossibleToFinish(TaskAttractiveness ta, int amount) {
+		
+	}
+	
+	def continueTask() {
+		
+	}
+	
+	
+	def step(def globalPrice) {
 		if(alive) {
 			if(hunger <=0 || thirst <= 0) {
 				alive = false
@@ -94,8 +153,17 @@ class Trader extends ReLogoTurtle {
 				thirst+=100
 				println("water drink")
 			}
+			
+			
+			if(task != null) {
+				continueTask()
+				return
+			}
+			
+			
 			if(water<panicThreshold || rice<panicThreshold) {
-				panic()
+				panic(globalPrice)
+				return
 			}
 			
 			
